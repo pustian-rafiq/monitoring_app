af99b7b8e78f2b5e45a3d0b78be1a64efc20882b
Md.Rafiqul <rafiqul.pust.cse@gmail.com>
2021-03-26 20:44:34 +0600

    set the server

---
+++ index.js
@@ -0,0 +1,48 @@
+/**
+ * Title: Uptime Monitoring Application
+ * Description: A RESTFul API to monitor up and down of user link
+ * Author: Md. Rafiqul Islam
+ * Date: 26/03/21
+ *
+ *
+ */
+
+//Dependencies
+const http = require('http');
+const url = require('url');
+
+// app object - module scafolding
+const app = {};
+
+// app config
+app.config = {
+    port: 3000
+};
+
+// create a server
+app.createServer = () => {
+    const server = http.createServer(app.handleReqRes);
+
+    server.listen(app.config.port, () => {
+        console.log(`The server is run ${app.config.port}`);
+
+    });
+}
+
+// handle request
+
+//get the parse url
+const parsedUrl = url.parse(req.url, true);
+const path = parsedUrl.pathname;
+
+
+// response handling
+app.handleReqRes = (req, res) => {
+    res.end("Hello world");
+
+
+}
+
+// start the server
+
+app.createServer();
